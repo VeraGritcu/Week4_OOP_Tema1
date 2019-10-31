b2e9d860d11624e8193692ab1cecb2d66ccd5024
Vera <Vera@DESKTOP-PBQVJHH>
2019-10-31 23:50:46 +0200

    uodate

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor p = new Professor();
+            p.Faculty = "Economics";
+            p.Name = "Mircea Radu";
+            p.Specialisation = "Trade, Travel and Services";
+
+            Student s = new Student();
+            s.Faculty = "Economics";
+            s.Name = "Anda Gheorge";
+            s.Year = 3;
+
+            Course c = new Course();
+            c.StudentList = new List<Student>();
+            c.StudentList.Add(s);
+            c.Professor = p;
+            c.Year = 3;
+            c.Name = "Marketing";
+            c.Print();
+
+        }
+    }
+}
